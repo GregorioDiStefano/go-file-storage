c1e164468217294648714bf75c026dfb34dc741a
Gregorio Di Stefano <greg.distefano+github@gmail.com>
2016-05-16 22:37:31 +0200

    Updated frontend

---
+++ static/js/main.js
@@ -0,0 +1,23 @@
+var app = angular.module('myUpload', ['ngFileUpload']);
+
+app.directive('customOnChange', function() {
+  return {
+    restrict: 'A',
+    link: function (scope, element, attrs) {
+      var onChangeHandler = scope.$eval(attrs.customOnChange);
+      element.bind('change', onChangeHandler);
+    }
+  };
+});
+
+
+app.controller('uploadCtrl', ['$scope', 'Upload', function($scope, Upload, $http) {
+
+  $scope.uploadFile = function(file) {
+      file.upload = Upload.upload({
+        method: 'PUT',
+        url: 'http://' + location.hostname,
+        data: {file: file},
+      });
+    }
+}]);
