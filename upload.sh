b59ed7782660318fcc92403024667b44f53630a4
Gregorio Di Stefano <greg.distefano+github@gmail.com>
2016-05-16 14:22:42 +0200

    Fixed upload.sh

--- upload.sh
+++ upload.sh
@@ -1,5 +1,6 @@
 FILENAME="random"
-
-openssl rand -out $FILENAME $(($RANDOM*1000))
-curl --upload $FILENAME http://localhost:8080/
+FILESIZE=$(($RANDOM*100))
+echo $(($FILESIZE/1024))kb
+openssl rand -out $FILENAME $FILESIZE
+curl --upload $FILENAME http://45.32.235.219:80/
 rm "$FILENAME"
