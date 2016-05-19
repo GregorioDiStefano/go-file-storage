4d630f6ecc7491d6a1af442bee0a64829f50b4bc
Gregorio Di Stefano <greg.distefano+github@gmail.com>
2016-05-19 21:30:04 +0200

    Frontend updated

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,9 @@
-var app = angular.module('myUpload', ['ngFileUpload']);
+var app = angular.module('myUpload', ['ngFileUpload', 'ui.bootstrap']);
+ app.config(function($interpolateProvider) {
+   $interpolateProvider.startSymbol('[[');
+   $interpolateProvider.endSymbol(']]');
+ });
+
 
 app.directive('customOnChange', function() {
   return {
@@ -10,14 +15,44 @@ app.directive('customOnChange', function() {
   };
 });
 
+var newPercent = 0;
+app.controller('uploadCtrl', ['$scope', 'Upload', "$http", "$timeout", function($scope, Upload, $http, $timeout) {
+  $scope.data = { progress : 0 };
 
-app.controller('uploadCtrl', ['$scope', 'Upload', function($scope, Upload, $http) {
+
+  (function progress(){
+         if($scope.data.progress < 100){
+             $timeout(function(){
+                 if (newPercent > 0) {
+                   $scope.data.progress = newPercent
+                 }
+                 progress();
+             },200);
+         }
+     })();
 
   $scope.uploadFile = function(file) {
       file.upload = Upload.http({
         method: 'PUT',
         url: 'http://' + location.hostname + "/" + file.name,
         data: file,
-      });
+      }).then(function (resp) {
+            console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data);
+        }, function (resp) {
+            console.log('Error status: ' + resp.status);
+        }, function (evt) {
+            var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+            newPercent = progressPercentage
+            //var appElement = document.querySelector('#progressUpload');
+            //var $scope = angular.element(appElement).scope();
+
+            //$scope.$apply(function() {
+            //    $scope.dynamic = 20;
+            //});
+
+            //update(progressPercentage)
+
+
+        });
     }
 }]);
--- upload.sh
+++ upload.sh
@@ -2,5 +2,5 @@ FILENAME="random"
 FILESIZE=$(($RANDOM*100))
 echo $(($FILESIZE/1024))kb
 openssl rand -out $FILENAME $FILESIZE
-curl --upload $FILENAME http://45.32.235.219:80/
+curl --upload $FILENAME http://$1/
 rm "$FILENAME"
