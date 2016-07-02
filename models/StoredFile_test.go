6ad59a1129186fa57bbc9f50837770a4b0b28cf2
Gregorio Di Stefano <greg.distefano+github@gmail.com>
2016-07-02 14:24:47 +0200

    Add some database tests

---
+++ models/StoredFile_test.go
@@ -0,0 +1,64 @@
+package models
+
+import (
+	"os"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func init() {
+	const (
+		DbFilename = "/tmp/files.testing.db"
+		Bucket     = "files"
+	)
+
+	//make sure the test db is clean
+	os.Remove("/tmp/files.testing.db")
+
+	var DB = Database{Filename: DbFilename, Bucket: Bucket}
+	DB.OpenDatabaseFile()
+}
+
+func TestFindUnusedKey(t *testing.T) {
+	//make helpers.RandomString return a key that exists,
+
+	//DB.OpenDatabaseFile()
+
+}
+
+func TestWriteStoredFile(t *testing.T) {
+	simpleStoredFiled := StoredFile{
+		Key:        "a",
+		DeleteKey:  "b",
+		FileName:   "filename",
+		FileSize:   2345,
+		UploadTime: time.Now().UTC(),
+		LastAccess: time.Now().UTC(),
+	}
+	DB.WriteStoredFile(simpleStoredFiled)
+}
+
+func TestReadStoredFile(t *testing.T) {
+	simpleStoredFiled := StoredFile{
+		Key:        "a1",
+		DeleteKey:  "b",
+		FileName:   "filename",
+		FileSize:   2345,
+		UploadTime: time.Now().UTC(),
+		LastAccess: time.Now().UTC(),
+	}
+	DB.WriteStoredFile(simpleStoredFiled)
+	assert.Equal(t, simpleStoredFiled, *DB.ReadStoredFile("a1"))
+}
+
+func TestGetAllKeys(t *testing.T) {
+	assert.Equal(t, []string{"a", "a1"}, *DB.GetAllKeys())
+}
+
+func TestDoesKeyExist(t *testing.T) {
+	for _, s := range *DB.GetAllKeys() {
+		assert.True(t, DB.DoesKeyExist(s))
+	}
+}
